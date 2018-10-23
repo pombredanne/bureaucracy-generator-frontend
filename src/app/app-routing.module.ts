0df1c436ead70b7aba22c96e047bcca1f100bce6
Václav Kozák <sudoman281@gmail.com>
2018-10-23 21:57:29 +0200

    Privacy policy forms (many bugs need to be fixed)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {PrivacyPolicyComponent} from './privacy-policy/privacy-policy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'privacyPolicy', component: PrivacyPolicyComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bureaucracy-generator-frontend';
+  title = 'Bureaucracy Generator';
 }
